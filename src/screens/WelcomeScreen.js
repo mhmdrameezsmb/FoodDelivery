0eb0c60e6dea265ededf61f89f46f39792dbd5ab
mhmdrameezsmb <rameez@newagesmb.com>
2022-08-05 09:14:09 +0530

    pagination_ok

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet, StatusBar, FlatList} from 'react-native';
-import {Colors, Fonts,General} from '../constants';
-import { WelcomeCard } from '../components';
-import { Display } from '../utils';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import {Colors, Fonts, General} from '../constants';
+import {WelcomeCard} from '../components';
+import {Display} from '../utils';
+import Separator from '../components/Separator';
 
-const Pagination =()=>{
-    return (
+const Pagination = () => {
+  return (
     <View style={styles.pageContainer}>
-            <View   style={styles.page} > </View>
-            <View   style={styles.page} > </View>
-            <View   style={styles.page} > </View>
-            <View   style={styles.page} > </View>
-        </View>
-}
-
-
+      <View style={styles.page}></View>
+      <View style={styles.page}></View>
+      <View style={styles.page}></View>
+    </View>
+  );
+};
 
 const WelcomeScreen = () => {
   return (
@@ -25,22 +31,31 @@ const WelcomeScreen = () => {
         translucent
       />
 
+      <Separator height={StatusBar.currentHeight} />
+      <Separator height={Display.setHeight(8)} />
+
       <View style={styles.welcomeListContainer}>
-        <FlatList 
-
-        data={General.WELCOME_CONTENTS}
-        keyExtractor={item  => item.title}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled={true}
-        overScrollMode="never"
-        renderItem={({item}) => <WelcomeCard {...item} />}
-
-
-        
+        <FlatList
+          data={General.WELCOME_CONTENTS}
+          keyExtractor={item => item.title}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled={true}
+          overScrollMode="never"
+          renderItem={({item}) => <WelcomeCard {...item} />}
         />
       </View>
-
+      <Separator height={Display.setHeight(8)} />
+      <Pagination />
+      <Separator height={Display.setHeight(8)}  />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity activeOpacity={0.8}>
+            <Text style={styles.buttonText}>SKIP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text style={styles.buttonText} activeOpacity={0.8}>NEXT</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -52,22 +67,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   welcomeListContainer: {
-    height: Display.setHeight(60)
-  },
-  titleText: {
-    color: Colors.DEFAULT_BLACK,
-    fontSize: 25,
-    fontFamily: Fonts.POPPINS_LIGHT,
+    height: Display.setHeight(60),
   },
   pageContainer: {
-  flexDirection:'row',
-   },
-  page: {
-    height:8,
-    width:15,
-    backgroundColor:Colors.DEFAULT_GREEN,
-    borderRadius:32,
+    flexDirection: 'row',
   },
+  page: {
+    height: 8,
+    width: 15,
+    backgroundColor: Colors.DEFAULT_GREEN,
+    borderRadius: 32,
+    marginHorizontal: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: Display.setWidth(90),
+    alignItems: 'center',
+  },
+buttonText: {
+    fontSize: 16,
+    fontFamily: Fonts.POPPINS_BOLD,
+    lineHeight: 16 * 1.4,
+  },
+  button: {
+    backgroundColor: Colors.LIGHT_GREEN,
+    paddingVertical: 20,
+    paddingHorizontal: 11,
+    borderRadius: 32,
+  },
+
+
+  
 });
 
 export default WelcomeScreen;
