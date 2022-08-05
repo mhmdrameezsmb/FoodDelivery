16d1228e522e5e46b7d66c613a894996286d7b7a
mhmdrameezsmb <rameez@newagesmb.com>
2022-08-05 07:41:27 +0530

    added separator

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -2,6 +2,19 @@ import React from 'react';
 import {View, Text, StyleSheet, StatusBar, FlatList} from 'react-native';
 import {Colors, Fonts,General} from '../constants';
 import { WelcomeCard } from '../components';
+import { Display } from '../utils';
+
+const Pagination =()=>{
+    return (
+    <View style={styles.pageContainer}>
+            <View   style={styles.page} > </View>
+            <View   style={styles.page} > </View>
+            <View   style={styles.page} > </View>
+            <View   style={styles.page} > </View>
+        </View>
+}
+
+
 
 const WelcomeScreen = () => {
   return (
@@ -19,12 +32,15 @@ const WelcomeScreen = () => {
         keyExtractor={item  => item.title}
         horizontal
         showsHorizontalScrollIndicator={false}
+        pagingEnabled={true}
+        overScrollMode="never"
         renderItem={({item}) => <WelcomeCard {...item} />}
 
 
         
         />
       </View>
+
     </View>
   );
 };
@@ -35,10 +51,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  welcomeListContainer: {
+    height: Display.setHeight(60)
+  },
   titleText: {
     color: Colors.DEFAULT_BLACK,
     fontSize: 25,
     fontFamily: Fonts.POPPINS_LIGHT,
+  },
+  pageContainer: {
+  flexDirection:'row',
+   },
+  page: {
+    height:8,
+    width:15,
+    backgroundColor:Colors.DEFAULT_GREEN,
+    borderRadius:32,
   },
 });
 
